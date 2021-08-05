1ae762e4f5cb7a22b8e838271ed145ab7f576fe2
habnet <mengeshaaabby1@gmail.com>
2021-08-05 14:19:44 -0400

    python-script has been structured

--- Day2/python-lesson.py
+++ Day2/python-lesson.py
@@ -1,3 +1,4 @@
+
 print('Python is awesome!')
 first_name = 'Fassica'
 last_name = 'Damtew'
@@ -5,8 +6,9 @@ country = 'USA'
 is_married = True
 course = 'Python Bootcamp'
 age = 25
-skills = ['XML', 'Excel', 'Data Analysis']
+skills = ['XML', 'Excel', 'Data Analysis','aws solution architect']
+year = 2021 
 full_name = first_name + ' ' + last_name 
-print(full_name, country, course, age, is_married)
+print(full_name, country, course, age, is_married, year , skills)
 print(5+6)
 print(type(3.1))
